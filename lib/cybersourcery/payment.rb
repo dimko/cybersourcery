55a1c0610a442d453cd865d4e3407ff29e9942fd
Dimko <deemox@gmail.com>
2015-02-09 18:11:41 +0300

    made payment more flexible

--- lib/cybersourcery/payment.rb
+++ lib/cybersourcery/payment.rb
@@ -4,35 +4,37 @@ module Cybersourcery
 
     IGNORE_FIELDS = [:commit, :utf8, :authenticity_token, :action, :controller]
 
-    attr_reader :params
+    attr_reader :params, :unsigned_field_names, :signed_date_time
 
     delegate :config, :sign, to: :Cybersourcery
     delegate :endpoint, to: :config
 
     def initialize(params)
+      @unsigned_field_names = params.delete(:unsigned_field_names) || config.unsigned_field_names
+      @signed_date_time = params.delete(:signed_date_time) || Time.now
       @params = params
     end
 
-    def default_params
+    def defaults
       {
         profile_id: config.profile_id,
         access_key: config.access_key,
         payment_method: config.payment_method,
         transaction_type: config.transaction_type,
+        unsigned_field_names: unsigned_field_names.join(','),
+        signed_date_time: signed_date_time.utc.strftime('%Y-%m-%dT%H:%M:%SZ'),
+        transaction_uuid: SecureRandom.uuid,
+        reference_number: SecureRandom.uuid,
+        signed_field_names: nil,
         locale: config.locale
       }
     end
 
     def signed_params
-      data = default_params.reverse_merge \
-        unsigned_field_names: config.unsigned_field_names.join(','),
-        transaction_uuid: SecureRandom.uuid,
-        reference_number: SecureRandom.uuid,
-        signed_date_time: current_time,
-        signed_field_names: nil
+      data = params.reverse_merge(defaults)
 
       data.delete_if do |key, _|
-        config.unsigned_field_names.include?(key) || IGNORE_FIELDS.include?(key)
+        unsigned_field_names.include?(key) || IGNORE_FIELDS.include?(key)
       end
 
       data[:signed_field_names] = data.keys.join(',')
@@ -41,15 +43,11 @@ module Cybersourcery
     end
 
     def fingerprint
-      @_fingerprint ||= Fingerprint.new(fingerprint_id)
+      @_fingerprint ||= Fingerprint.new(fingerprint_id) if fingerprint_id
     end
 
     def fingerprint_id
-      SecureRandom.uuid
-    end
-
-    def current_time
-      Time.now.utc.strftime('%Y-%m-%dT%H:%M:%SZ')
+      params[:device_fingerprint_id]
     end
   end
 end
